3a4808e7958c40847e7c5b17c8addd4e6b0a2c1a
unknown <priya.md0002@gmail.com>
2020-09-27 15:01:30 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+ECHO "Hello World!<br>";
+echo "Hello World!<br>";
+EcHo "Hello World!<br>";
+?>
+
+</body>
+</html>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php
+	/*zznnxzk)*/
+	?>
+	// single line comment
+
+</body>
+</html>
